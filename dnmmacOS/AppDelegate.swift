5e949237699c3f9e3a0d17de099aa36a7d13d151
James <jbean46@gmail.com>
2017-01-04 22:00:01 +0100

    Create project

---
+++ dnmmacOS/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  dnmmacOS
+//
+//  Created by James Bean on 1/4/17.
+//  Copyright © 2017 James Bean. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
